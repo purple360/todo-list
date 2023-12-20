cf7380b81829b517fe056839129520fe03a02bf8
unknown <francisbrobbey090@gmail.com>
2023-12-20 21:42:55 +0000

    todo

--- todo/settings.py
+++ todo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-dmx2kuii!&r9%t83s160d(-sxk*k_&^x63+s&)m$8a!3374atu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
